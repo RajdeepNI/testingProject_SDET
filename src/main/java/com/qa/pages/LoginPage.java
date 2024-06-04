0fa57062c63b108d93e8ea8c82cb8ea37c9a96ef
Rajdeep Gupta <rajdeep.gupta1001@gmail.com>
2024-06-04 12:56:22 +0530

    TestBase class created

--- src/main/java/com/qa/pages/LoginPage.java
+++ src/main/java/com/qa/pages/LoginPage.java
@@ -1,6 +1,19 @@
 package com.qa.pages;
 
-public class LoginPage {
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
+import com.qa.testbase.TestBase;
+
+public class LoginPage extends TestBase {
+	
+	//PageFactory  - Object Repository
+	/*Locators*/
+	@FindBy(linkText = "Login")public WebElement loginBtn;
+	//42:26
+	
+	
+	
+	/*Automation Script*/
 	
 }
